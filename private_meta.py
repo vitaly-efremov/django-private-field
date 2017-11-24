db1502eb5b50d05d86d8774f7467e74a0bf92323
vefremov <efremov.vitaly@gmail.com>
2017-11-24 21:05:08 +0700

    Fixed encapsulation problem. Changed the docstring.

--- private_meta.py
+++ private_meta.py
@@ -10,22 +10,8 @@ class PrivateFieldsMeta(models.base.ModelBase):
     >>> class Employee(models.Model, metaclass=PrivateFieldsMeta):
     >>>     name = models.CharField(max_length=256)
     >>>     rate = models.FloatField(default=0)
-    >>>
     >>>     class Meta:
     >>>         private_fields = ('rate', )
-    >>>
-    >>>
-    >>> Employee(name='John', rate=15)
-    >>> TypeError: 'rate' is an invalid keyword argument for this function
-    >>>
-    >>> employee = Employee(name='John')
-    >>> employee.rate
-    >>>
-    >>> AttributeError: 'Employee' object has no attribute 'rate'
-    >>> employee._rate
-    >>>
-    >>> 0
-    >>> Employee.objects.filter(rate=12)
     """
     def __new__(cls, name, bases, attrs):
         attr_meta = attrs.pop('Meta', None)
@@ -33,7 +19,7 @@ class PrivateFieldsMeta(models.base.ModelBase):
         private_fields = getattr(attr_meta, 'private_fields', ())
         for private_field_name in private_fields:
             field = attrs.pop(private_field_name, None)
-            if field:
+            if field and hasattr(field, 'db_column'):
                 field.db_column = private_field_name
                 attrs['_%s' % private_field_name] = field
 
